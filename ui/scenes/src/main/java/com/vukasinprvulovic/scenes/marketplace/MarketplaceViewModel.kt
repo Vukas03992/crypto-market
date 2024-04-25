ba002124894625d39ddf1410e5560d992a31c091
vukasin <vukas03992@gmail.com>
2024-04-26 00:40:41 +0200

    Reorganize viewModel

--- ui/scenes/src/main/java/com/vukasinprvulovic/scenes/marketplace/MarketplaceViewModel.kt
+++ ui/scenes/src/main/java/com/vukasinprvulovic/scenes/marketplace/MarketplaceViewModel.kt
@@ -1,25 +1,15 @@
 package com.vukasinprvulovic.scenes.marketplace
 
-import android.util.Log.e
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vukasinprvulovic.application.entities.trading.data.price.Price
-import com.vukasinprvulovic.application.entities.trading.data.price.price
-import com.vukasinprvulovic.application.entities.trading.pair.TradingPairs
 import com.vukasinprvulovic.application.features.cryptomarketplace.CryptoMarketplace
-import com.vukasinprvulovic.application.features.cryptomarketplace.result.CryptoMarketplaceResults
-import com.vukasinprvulovic.application.features.cryptomarketplace.result.findDataOfInstance
-import com.vukasinprvulovic.application.features.cryptomarketplace.store.components.SearchedTradingPairs
 import com.vukasinprvulovic.scenes.marketplace.scene.MarketplaceViewEvent
 import com.vukasinprvulovic.scenes.marketplace.scene.MarketplaceViewState
-import com.vukasinprvulovic.scenes.marketplace.scene.models.TradingPairModel
+import com.vukasinprvulovic.scenes.marketplace.scene.state.produceMarketplaceViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.math.BigDecimal
-import java.math.RoundingMode
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,41 +22,7 @@ class MarketplaceViewModel @Inject constructor(
 
     init {
         cryptoMarketplace.subscribe(viewModelScope) {
-            e("TAG", it.toString())
-            val data = it.data()
-            val errors = it.errors()
-            val isFinalDataReceived = data.findDataOfInstance<CryptoMarketplaceResults.FinalData>()
-            val isLoading = isFinalDataReceived == null && errors.isEmpty()
-            val tradingPairs = data.findDataOfInstance<TradingPairs>()
-            val searchedTradingPairs = data.findDataOfInstance<SearchedTradingPairs>()
-
-            if (tradingPairs == null && searchedTradingPairs == null) {
-                internalMarketplaceState.value = internalMarketplaceState.value.copy(isLoading = isLoading, error = errors)
-            } else {
-                val pairs = searchedTradingPairs?.results ?: tradingPairs
-                internalMarketplaceState.value = internalMarketplaceState.value.copy(
-                    isLoading = isLoading,
-                    searchingToken = searchedTradingPairs?.searchingToken ?: "",
-                    pairs = pairs?.map { tradingPair ->
-                        val dailyChanges = tradingPair.tradingData.getData<Price.Change.Daily>()
-                        TradingPairModel(
-                            searchedTradingPairs?.searchingToken ?: "",
-                            tradingPair.baseCurrency.properties.name,
-                            tradingPair.baseCurrency.properties.icon ?: "",
-                            tradingPair.ticker.symbol,
-                            tradingPair.tradingData.price().let {
-                                if (it == 0f) "" else formatToMaxFourDecimals(it)
-                            },
-                            tradingPair.quoteCurrency.properties.symbol,
-                            (dailyChanges.percentageChangedSinceYesterday * 100f).let {
-                                if (it == 0f) "" else formatToMaxFourDecimals(it)
-                            },
-                            dailyChanges.percentageChangedSinceYesterday >= 0
-                        )
-                    } ?: internalMarketplaceState.value.pairs,
-                    error = it.errors()
-                )
-            }
+            internalMarketplaceState.value = produceMarketplaceViewState(internalMarketplaceState.value, it)
         }
     }
 
@@ -83,16 +39,5 @@ class MarketplaceViewModel @Inject constructor(
             internalMarketplaceState.value = internalMarketplaceState.value.copy(isLoading = true)
             cryptoMarketplace.search(token)
         }
-    }
-
-    private fun formatToMaxFourDecimals(number: Float): String {
-        val bigDecimal = BigDecimal(number.toDouble())
-        val scaled = bigDecimal.setScale(4, RoundingMode.HALF_UP)
-        val formattedString = scaled.toPlainString()
-        val decimalPointIndex = formattedString.indexOf('.')
-        if (decimalPointIndex != -1) {
-            return formattedString.trimEnd('0').trimEnd('.')
-        }
-        return formattedString
     }
 }
